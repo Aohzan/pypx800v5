d9970feef7a18df1db0e6930d694eb7e866b3d76
Matthieu Bourgain <matthieu.bourgain@fr.clara.net>
2021-11-30 22:19:21 +0100

    rollback count

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pypx800v5",
-    version="0.5.0",
+    version="0.5.1",
     author="Aohzan",
     author_email="aohzan@gmail.com",
     description="Control the IPX800V5 and its extensions.",
