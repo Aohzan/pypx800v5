02ec6df2d0afb3966a9750795c59c5c6542cd757
Matthieu Bourgain <matthieu.bourgain@fr.clara.net>
2022-10-06 20:16:33 +0200

    fix export constants

--- pypx800v5/__init__.py
+++ pypx800v5/__init__.py
@@ -1,6 +1,32 @@
 """Asynchronous Python client for the IPX800 v5 API."""
 
 
+from .const import (
+    API_CONFIG_ID,
+    API_CONFIG_NAME,
+    API_CONFIG_PARAMS,
+    API_CONFIG_TYPE,
+    EXT_X4FP,
+    EXT_X4VR,
+    EXT_X8D,
+    EXT_X8R,
+    EXT_X010V,
+    EXT_X24D,
+    EXT_XDIMMER,
+    EXT_XPWM,
+    EXT_XTHL,
+    EXTENSIONS,
+    IPX,
+    OBJECT_COUNTER,
+    OBJECT_PUSH,
+    OBJECT_TEMPO,
+    OBJECT_THERMOSTAT,
+    OBJECT_TIMER,
+    OBJECTS,
+    TYPE_ANA,
+    TYPE_IO,
+    TYPE_STR,
+)
 from .counter import Counter
 from .exceptions import (
     IPX800CannotConnectError,
@@ -28,7 +54,6 @@ from .xpwm import XPWM
 from .xthl import XTHL
 
 __all__ = [
-    "Counter",
     "IPX800CannotConnectError",
     "IPX800InvalidAuthError",
     "IPX800RequestError",
@@ -38,6 +63,8 @@ __all__ = [
     "IPX800OptoInput",
     "IPX800Relay",
     "IPX800OpenColl",
+    "IPX",
+    "Counter",
     "Tempo",
     "Thermostat",
     "X010V",
@@ -50,4 +77,26 @@ __all__ = [
     "XDimmer",
     "XPWM",
     "XTHL",
+    "EXTENSIONS",
+    "EXT_X4VR",
+    "EXT_X010V",
+    "EXT_X24D",
+    "EXT_X4FP",
+    "EXT_X8D",
+    "EXT_X8R",
+    "EXT_XDIMMER",
+    "EXT_XPWM",
+    "EXT_XTHL",
+    "OBJECTS" "OBJECT_COUNTER",
+    "OBJECT_PUSH",
+    "OBJECT_TEMPO",
+    "OBJECT_THERMOSTAT",
+    "OBJECT_TIMER",
+    "TYPE_ANA",
+    "TYPE_IO",
+    "TYPE_STR",
+    "API_CONFIG_NAME",
+    "API_CONFIG_ID",
+    "API_CONFIG_PARAMS",
+    "API_CONFIG_TYPE",
 ]
